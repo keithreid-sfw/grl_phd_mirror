5a9b600f9b5719e8dcf32ce4c12e8abbd1d0e377
keithreid-sfw <56557748+keithreid-sfw@users.noreply.github.com>
2025-03-31 14:55:03 +0100

    Add files via upload

--- shell.nix
+++ shell.nix
@@ -1,6 +1,45 @@
-{ pkgs ? import <nixpkgs> {}
+# shell.nix
+
+{ pkgs ? import <nixpkgs> {} 
+
 }:
 
-pkgs.rstudioWrapper.override {
-  packages = with pkgs.rPackages; [ bslib dplyr ggplot2 partitions shiny ];
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-24.11.tar.gz") {};
+in pkgs.mkShell {
+
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+      # select Python packages here
+      # collections # collections is base library so already exists
+      # itertools   # itertools is base library so already exists
+      # math        # math is base library so already exists  
+      matplotlib
+      numpy
+      pandas
+    ]))
+    
+    (pkgs.rstudioWrapper.override {
+      packages = with pkgs.rPackages; [
+      # bslib
+      comprehenr
+      partitions
+      # rstudio doesnt need to be be here because rstudio is in the wrapper
+      shiny
+      ];
+    })
+  
+  ];
+  
+
+    
+  GREETING = "Hello this environment has Python and R!";
+  
+  shellHook = ''
+  echo $GREETING
+  '';
+
 }
+
+# could be a fix for ubuntu at https://github.com/numtide/nix-gl-host
+
